0405b2404b0ef7ca8ec913a8fda54e5a051172e8
furkan <furkan.dolek@cern.ch>
2020-01-17 17:32:32 +0100

    baseline

--- spy.py
+++ spy.py
@@ -1,14 +1,10 @@
+import matplotlib.pyplot as plt
+from scipy.misc import electrocardiogram
+from scipy.signal import find_peaks
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-#%matplotlib inline
-#with open("waveforms_ch0_run421.txt") as m:
-#with open("waveforms_ch0_run421.txt") as f:
-#    print(f.readline())
-    
-    
-df = pd.read_csv("waveforms_ch2_run443.txt",
+
+df = pd.read_csv("../waveform/waveforms_ch0_run454.txt",
                  delim_whitespace=True,
-                 usecols=[1,],
-                 names=["t",])
-df.plot()
+                 usecols=[1,])
+df.to_numpy()
