1b12fafa794e2c888733b3f6c78cba2382aec707
furkan dolek <furkan.dolek@cern.ch>
2020-01-24 12:24:06 +0100

    source env

--- envi.sh
+++ envi.sh
@@ -1,4 +1,4 @@
-source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7/setup.sh
 source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.18.02/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 export EOS_MGM_URL=root://eosproject.cern.ch
 export LD_LIBRARY_PATH=~/rootlibs:$LD_LIBRARY_PATH
